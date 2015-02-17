df7c747d66938503892b85ec30dcc331c996dca1
Threstle <Etienne.Chaumont@gmail.com>
2015-02-17 21:37:56 +0100

    nooope

--- server.js
+++ server.js
@@ -32,7 +32,7 @@ app.listen(8080)
 var express = require('express')
   , stylus = require('stylus')
   , nib = require('nib')
- 
+
 var app = express();
 
 // set the port of our application
@@ -40,7 +40,7 @@ var app = express();
 var port = process.env.PORT || 8080;
 
 // set the view engine to ejs
-app.set('view engine', 'ejs');
+app.set('view engine', 'jade');
 
 // make express look in the public directory for assets (css/js/img)
 app.use(express.static(__dirname + '/public'));
@@ -49,7 +49,7 @@ app.use(express.static(__dirname + '/public'));
 app.get('/', function(req, res) {
 
 	// ejs render automatically looks in the views folder
-	res.render('index');
+	res.render('index','rightSide')
 });
 
 app.listen(port, function() {
