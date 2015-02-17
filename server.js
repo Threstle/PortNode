2de7c27bc85032d34bc04f1e67def730efa36f99
Threstle <Etienne.Chaumont@gmail.com>
2015-02-17 20:25:10 +0100

    test package.json

--- server.js
+++ server.js
@@ -1,23 +1,30 @@
-var express = require('express');
-var app = express();
+/*
+ * Module dependencies
+ */
+var express = require('express')
+  , stylus = require('stylus')
+  , nib = require('nib')
 
-// set the port of our application
-// process.env.PORT lets the port be set by Heroku
 var port = process.env.PORT || 8080;
+var app = express()
+function compile(str, path) {
+  return stylus(str)
+    .set('filename', path)
+    .use(nib())
+}
+app.set('views', __dirname + '/views')
+app.set('view engine', 'jade')
 
-// set the view engine to ejs
-app.set('view engine', 'ejs');
+app.use(stylus.middleware(
+  { src: __dirname + '/public'
+  , compile: compile
+  }
+))
 
-// make express look in the public directory for assets (css/js/img)
-app.use(express.static(__dirname + '/public'));
+app.use(express.static(__dirname + '/public'))
 
-// set the home page route
-app.get('/', function(req, res) {
-
-	// ejs render automatically looks in the views folder
-	res.render('index');
-});
-
-app.listen(port, function() {
-	console.log('Our app is running on http://localhost:' + port);
-});
+app.get('/', function (req, res) {
+  res.render('index','rightSide'
+  )
+})
+app.listen(8080)
