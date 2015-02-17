c1d7d11c65790e8826abaae023d07339a6032d15
Threstle <Etienne.Chaumont@gmail.com>
2015-02-17 21:34:23 +0100

    no comprendo

--- server.js
+++ server.js
@@ -28,3 +28,30 @@ app.get('/', function (req, res) {
   )
 })
 app.listen(8080)
+
+var express = require('express')
+  , stylus = require('stylus')
+  , nib = require('nib')
+ 
+var app = express();
+
+// set the port of our application
+// process.env.PORT lets the port be set by Heroku
+var port = process.env.PORT || 8080;
+
+// set the view engine to ejs
+app.set('view engine', 'ejs');
+
+// make express look in the public directory for assets (css/js/img)
+app.use(express.static(__dirname + '/public'));
+
+// set the home page route
+app.get('/', function(req, res) {
+
+	// ejs render automatically looks in the views folder
+	res.render('index');
+});
+
+app.listen(port, function() {
+	console.log('Our app is running on http://localhost:' + port);
+});
