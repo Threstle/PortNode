7b5b85f222841b466bf699c99c34bd462ce94180
Threstle <Etienne.Chaumont@gmail.com>
2015-02-18 21:38:33 +0100

    test git

--- server.js
+++ server.js
@@ -49,7 +49,7 @@ app.use(express.static(__dirname + '/public'));
 app.get('/', function(req, res) {
 
 	// ejs render automatically looks in the views folder
-	res.render('aboutPage','rightSide')
+	res.render('aboutPage','layout')
 });
 
 app.listen(port, function() {
