ec3047e65ac47406ee24200a7a84ca253c9bbd6d
Threstle <Etienne.Chaumont@gmail.com>
2015-02-18 21:34:45 +0100

    change css & layout

--- server.js
+++ server.js
@@ -24,8 +24,34 @@ app.use(stylus.middleware(
 app.use(express.static(__dirname + '/public'))
 
 app.get('/', function (req, res) {
-  res.render('rightSide','layout'
+  res.render('aboutPage','rightSide'
   )
 })
 app.listen(8080)
 
+var express = require('express')
+  , stylus = require('stylus')
+  , nib = require('nib')
+
+var app = express();
+
+// set the port of our application
+// process.env.PORT lets the port be set by Heroku
+var port = process.env.PORT || 8080;
+
+// set the view engine to ejs
+app.set('view engine', 'jade');
+
+// make express look in the public directory for assets (css/js/img)
+app.use(express.static(__dirname + '/public'));
+
+// set the home page route
+app.get('/', function(req, res) {
+
+	// ejs render automatically looks in the views folder
+	res.render('index','rightSide')
+});
+
+app.listen(port, function() {
+	console.log('Our app is running on http://localhost:' + port);
+});
