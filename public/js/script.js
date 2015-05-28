14f2fdf6d6fe7a766335b50b01a3430d3b6eec37
Threstle <Etienne.Chaumont@gmail.com>
2015-05-28 22:00:57 +0200

    begin responsive version

--- public/js/script.js
+++ public/js/script.js
@@ -8,7 +8,7 @@ $(function() {
  
 
   $( document ).ready(function(){
-      $('.sousSection').css('height',$(window).height());
+      $('.sousSection').css('min-height',$(window).height());
        $('#loader').delay(2000).fadeOut(200);
        $('#loader h1').css('marginTop',$(window).height()/2-50);
   });
@@ -152,7 +152,7 @@ $(function() {
       //})
 
 
-    },1000);
+    },100000);
 
     //Prototypes
     String.prototype.replaceAt=function(index, character) {
